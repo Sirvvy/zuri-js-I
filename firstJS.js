eebda842f2e10473a51df20c6f166dff5ae5ca2d
sirvvy <henry.wills.dev@gmail.com>
2022-06-25 12:25:36 +0100

    first commit

---
+++ firstJS.js
@@ -0,0 +1,15 @@
+var firstname = "Henry-williams";
+var middlename = "Chibueze";
+var lastname = "Opara";
+var height = "172cm";
+var country = "Nigeria";
+
+
+
+// alert('My name is' + ' ',firstname + ' ' + middlename + ' ' + lastname);
+// alert('I am' + ' ', height);
+// alert(' I am ' + ' ' + country);
+
+alert(' My Name is ' + ' ' + firstname + ' ' + middlename + ' ' + lastname);
+alert(' I am ' + height + ' in heignt ');
+alert(' I am ' + ' from ' + country);
