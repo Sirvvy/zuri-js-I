ec579ed3d4a17dd534cb479eee697269485dcf6a
sirvvy <henry.wills.dev@gmail.com>
2022-06-25 12:27:26 +0100

    second commit

--- firstJS.js
+++ firstJS.js
@@ -6,10 +6,6 @@ var country = "Nigeria";
 
 
 
-// alert('My name is' + ' ',firstname + ' ' + middlename + ' ' + lastname);
-// alert('I am' + ' ', height);
-// alert(' I am ' + ' ' + country);
-
 alert(' My Name is ' + ' ' + firstname + ' ' + middlename + ' ' + lastname);
 alert(' I am ' + height + ' in heignt ');
 alert(' I am ' + ' from ' + country);
